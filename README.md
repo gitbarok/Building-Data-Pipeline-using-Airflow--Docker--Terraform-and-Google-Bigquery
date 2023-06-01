8f5e86cca7275eaed0899c263f3966c75fcdfba3
gitbarok <muhammadrizkymubarok@gmail.com>
2023-06-01 20:55:49 +0800

    add readme and change variable

---
+++ README.md
@@ -0,0 +1,7 @@
+# Building Data Pipeline Using Airflow, Docker, Terraform and Google Bigquery
+
+![Project Diagram](docs/diagram.png)
+
+## For the tutorial
+
+Soon on my blog rizkybarok.com
--- airflow/dags/helpers/local_to_bq.py
+++ airflow/dags/helpers/local_to_bq.py
@@ -8,8 +8,9 @@ from google.oauth2 import service_account
 def load_to_bq(filename):
     # Define Variable
     SERVICE_ACCOUNT_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-    PROJECT_ID = "tactile-vehicle-351405"
-    TABLE_ID = f"{PROJECT_ID}.jti_ulm_scrape.jtiulm_paper_details"
+    PROJECT_ID = "FILL WITH YOUR PROJECT ID HERE"
+    # ALL U NEED IS CREATE DATASET MANUALLY OR WITH TERRAFORM, AND JUST DEFINE TABLE NAME THAT YOU WANT, BIGQUERY WILL AUTOMATICALLY CREATE TABLE FOR U
+    TABLE_ID = f"{PROJECT_ID}.[CHANGE TO YOUR DATASET NAME].[YOUR TABLE NAME IT WILL CREATE AUTOMATICALLY]"
 
     df = pd.read_csv(filename)
 
